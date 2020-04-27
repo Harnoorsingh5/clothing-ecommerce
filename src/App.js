c02e0c87e98bc25b4b9ba574ece2fa0b1d1062e7
Harnoor Singh <harnoor.singh539@gmail.com>
2020-04-26 23:50:41 -0400

    menu item routing added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,48 @@
 import React from 'react';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component.jsx'
+import {Switch, Route} from 'react-router-dom';
+
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
+)
+const JacketsPage = () => (
+  <div>
+    <h1>JACKETS PAGE</h1>
+  </div>
+)
+const SneakersPage = () => (
+  <div>
+    <h1>SNEAKERS PAGE</h1>
+  </div>
+)
+const WomensPage = () => (
+  <div>
+    <h1>WOMENS PAGE</h1>
+  </div>
+)
+const MensPage = () => (
+  <div>
+    <h1>MENS PAGE</h1>
+  </div>
+)
+
+// moment that route inside switch matches it does not renders anything else
+// it gives control over code; more security in terms it will only opn that particular link 
 function App() {
   return (
-    <HomePage/>
+    <div>
+      <Switch> 
+        <Route exact path='/' component={HomePage}/>
+        <Route path='/shop/hats' component={HatsPage}/>
+        <Route path='/shop/jackets' component={JacketsPage}/>
+        <Route path='/shop/sneakers' component={SneakersPage}/>
+        <Route path='/shop/womens' component={WomensPage}/>
+        <Route path='/shop/mens' component={MensPage}/>
+      </Switch>
+    </div>
   );
 }
 
